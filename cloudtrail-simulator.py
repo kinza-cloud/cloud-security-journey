38f62359dd3c6bf29c8c6e1e65f6e02f45462fdf
kinza-cloud <kinzajabeen167@gmail.com>
2026-08-24 19:29:17 +0500

    Create cloudtrail-simulator.py

---
+++ cloudtrail-simulator.py
@@ -0,0 +1,9 @@
+# CloudTrail Simulator – Logs user actions
+
+user = input("Enter the name of the user: ")
+action = input("What action did you perform? (read/write/delete): ")
+
+print("\n📋 CloudTrail Log:")
+print("User:", user)
+print("Action:", action)
+print("Status: Logged successfully ✅")
