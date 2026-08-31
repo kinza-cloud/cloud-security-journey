731750194d643beab9c5b0d406d1e914267f6960
kinza-cloud <kinzajabeen167@gmail.com>
2026-08-31 16:26:04 +0500

    Create lambda-simulator.py

---
+++ lambda-simulator.py
@@ -0,0 +1,8 @@
+event = input("Enter event type (s3_upload, api_call, scheduled): ")
+name = input("Enter your name: ")
+
+print("\n Lambda Function Triggered")
+print("Event Type:", event)
+print("Triggered By:", name)
+print("Status: Function executed successfully.")
+print("Timestamp: 2026-08-31 10:00 AM")
