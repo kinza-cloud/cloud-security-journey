730fbbeb640c61b84500793e526bab3fa353160b
kinza-cloud <kinzajabeen167@gmail.com>
2026-09-04 18:37:23 +0500

    Create cloudformation-simulator.py

---
+++ cloudformation-simulator.py
@@ -0,0 +1,9 @@
+# CloudFormation Simulator
+
+resource_type = input("Enter resource type (S3, EC2, Lambda): ")
+resource_name = input("Enter resource name: ")
+
+print("\nCloudFormation Stack Created")
+print("Resource Type:", resource_type)
+print("Resource Name:", resource_name)
+print("Status: Stack creation initiated")
