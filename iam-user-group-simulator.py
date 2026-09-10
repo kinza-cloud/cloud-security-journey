bc39a810de5bb9329b70049e0c2f50f05e5bd7e0
kinza-cloud <kinzajabeen167@gmail.com>
2026-09-10 16:55:35 +0500

    Create iam-user-group-simulator.py

---
+++ iam-user-group-simulator.py
@@ -0,0 +1,11 @@
+# IAM User + Group Simulator
+
+username = input("Enter username: ")
+group = input("Enter group name: ")
+policy = input("Enter policy name: ")
+
+print("\nIAM User Created")
+print("Username:", username)
+print("Added to Group:", group)
+print("Policy Attached:", policy)
+print("Status: User added successfully")
